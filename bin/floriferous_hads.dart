ab416fbdca37cf0f256d2ed2b50fe5dfc04d578b
Jose Carlos Jimenez <jimenez.jcarlos28@gmail.com>
2023-03-27 17:44:33 -0700

    subiendo proyecto a git

---
+++ bin/floriferous_hads.dart
@@ -0,0 +1,5 @@
+import 'package:floriferous_hads/floriferous_hads.dart' as floriferous_hads;
+
+void main(List<String> arguments) {
+  print('Hello world: ${floriferous_hads.calculate()}!');
+}
---
+++ test/floriferous_hads_test.dart
@@ -0,0 +1,8 @@
+import 'package:floriferous_hads/floriferous_hads.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
