d224a0b0b09770c518688b3f480f0369153b3d03
JoCaJimenez28 <jimenez.jcarlos28@gmail.com>
2023-03-28 23:21:56 -0700

    generando cartas objetivo

--- test/floriferous_hads_test.dart
+++ test/floriferous_hads_test.dart
@@ -8,12 +8,12 @@ void main() {
 
   test('obtener carta lily', () {
     Carta cartaLily = new Carta();
-    expect(cartaLily.cartasFlor[5].toString(), '[Lily, White, Ladybug]');
+    expect(cartaLily.cartasFlor[5].toString(), '[Lily, White, Ladybug, flor]');
   });
 
   test('obtener carta daisy', () {
     Carta cartaLily = new Carta();
-    expect(cartaLily.cartasFlor[13].toString(), '[Daisy, Pink, sinBug]');
+    expect(cartaLily.cartasFlor[13].toString(), '[Daisy, Pink, sinBug, flor]');
   });
 }
 
@@ -23,39 +23,45 @@ class Carta {
   final List<String> flor = ['Daisy','Lily','Mum','Puppy','Tulip'];
   final List<String> color = ['White','Yellow','Orange','Pink','Purple'];
   final List<String> bug = ['Bee','Beetle','Butterfly','Ladybug','Moth', 'sinBug'];
-  late final List<String> tipoCarta = ['normal', 'objetivo', 'deseos'];
+  final List<String> tipoCarta = ['flor', 'objetivo', 'deseos'];
   late final List<List<String>> cartasFlor = [
     //lily
-    [flor[1], color[0],bug[5]], [flor[1], color[1],bug[5]],
-    [flor[1], color[2],bug[5]], [flor[1], color[3],bug[5]],
-    [flor[1], color[4],bug[5]], [flor[1], color[0],bug[3]],
-    [flor[1], color[1],bug[1]], [flor[1], color[2],bug[2]],
-    [flor[1], color[3],bug[4]], [flor[1], color[4],bug[0]],
+    [flor[1], color[0],bug[5], tipoCarta[0]], [flor[1], color[1],bug[5], tipoCarta[0]],
+    [flor[1], color[2],bug[5], tipoCarta[0]], [flor[1], color[3],bug[5], tipoCarta[0]],
+    [flor[1], color[4],bug[5], tipoCarta[0]], [flor[1], color[0],bug[3], tipoCarta[0]],
+    [flor[1], color[1],bug[1], tipoCarta[0]], [flor[1], color[2],bug[2], tipoCarta[0]],
+    [flor[1], color[3],bug[4], tipoCarta[0]], [flor[1], color[4],bug[0], tipoCarta[0]],
     //daisy
-    [flor[0], color[0],bug[5]], [flor[0], color[1],bug[5]],
-    [flor[0], color[2],bug[5]], [flor[0], color[3],bug[5]],
-    [flor[0], color[4],bug[5]], [flor[0], color[0],bug[3]],
-    [flor[0], color[1],bug[1]], [flor[0], color[2],bug[0]],
-    [flor[0], color[3],bug[3]], [flor[0], color[4],bug[4]],
+    [flor[0], color[0],bug[5], tipoCarta[0]], [flor[0], color[1],bug[5], tipoCarta[0]],
+    [flor[0], color[2],bug[5], tipoCarta[0]], [flor[0], color[3],bug[5], tipoCarta[0]],
+    [flor[0], color[4],bug[5], tipoCarta[0]], [flor[0], color[0],bug[3], tipoCarta[0]],
+    [flor[0], color[1],bug[1], tipoCarta[0]], [flor[0], color[2],bug[0], tipoCarta[0]],
+    [flor[0], color[3],bug[3], tipoCarta[0]], [flor[0], color[4],bug[4], tipoCarta[0]],
     //Mum
-    [flor[2], color[0],bug[5]], [flor[2], color[1],bug[5]],
-    [flor[2], color[2],bug[5]], [flor[2], color[3],bug[5]],
-    [flor[2], color[4],bug[5]], [flor[2], color[0],bug[3]],
-    [flor[2], color[1],bug[4]], [flor[2], color[2],bug[1]],
-    [flor[2], color[3],bug[0]], [flor[2], color[4],bug[2]],
+    [flor[2], color[0],bug[5], tipoCarta[0]], [flor[2], color[1],bug[5], tipoCarta[0]],
+    [flor[2], color[2],bug[5], tipoCarta[0]], [flor[2], color[3],bug[5], tipoCarta[0]],
+    [flor[2], color[4],bug[5], tipoCarta[0]], [flor[2], color[0],bug[3], tipoCarta[0]],
+    [flor[2], color[1],bug[4], tipoCarta[0]], [flor[2], color[2],bug[1], tipoCarta[0]],
+    [flor[2], color[3],bug[0], tipoCarta[0]], [flor[2], color[4],bug[2], tipoCarta[0]],
     //Poppy
-    [flor[3], color[0],bug[5]], [flor[3], color[1],bug[5]],
-    [flor[3], color[2],bug[5]], [flor[3], color[3],bug[5]],
-    [flor[3], color[4],bug[5]], [flor[3], color[0],bug[4]],
-    [flor[3], color[1],bug[0]], [flor[3], color[2],bug[3]],
-    [flor[3], color[3],bug[2]], [flor[3], color[4],bug[1]],
+    [flor[3], color[0],bug[5], tipoCarta[0]], [flor[3], color[1],bug[5], tipoCarta[0]],
+    [flor[3], color[2],bug[5], tipoCarta[0]], [flor[3], color[3],bug[5], tipoCarta[0]],
+    [flor[3], color[4],bug[5], tipoCarta[0]], [flor[3], color[0],bug[4], tipoCarta[0]],
+    [flor[3], color[1],bug[0], tipoCarta[0]], [flor[3], color[2],bug[3], tipoCarta[0]],
+    [flor[3], color[3],bug[2], tipoCarta[0]], [flor[3], color[4],bug[1], tipoCarta[0]],
     //Tulip
-    [flor[4], color[0],bug[5]], [flor[4], color[1],bug[5]],
-    [flor[4], color[2],bug[5]], [flor[4], color[3],bug[5]],
-    [flor[4], color[4],bug[5]], [flor[4], color[0],bug[0]],
-    [flor[4], color[1],bug[2]], [flor[4], color[2],bug[4]],
-    [flor[4], color[3],bug[1]], [flor[4], color[4],bug[3]]
+    [flor[4], color[0],bug[5], tipoCarta[0]], [flor[4], color[1],bug[5], tipoCarta[0]],
+    [flor[4], color[2],bug[5], tipoCarta[0]], [flor[4], color[3],bug[5], tipoCarta[0]],
+    [flor[4], color[4],bug[5], tipoCarta[0]], [flor[4], color[0],bug[0], tipoCarta[0]],
+    [flor[4], color[1],bug[2], tipoCarta[0]], [flor[4], color[2],bug[4], tipoCarta[0]],
+    [flor[4], color[3],bug[1], tipoCarta[0]], [flor[4], color[4],bug[3], tipoCarta[0]]
   ];
 
+  late final List<List<String>> cartaObjetivo = [
+    [flor[4], bug[3], bug[2]], [flor[0], flor[2], bug[0]], [flor[2], flor[2], bug[2]],
+    [flor[4], flor[4], flor[2]], [flor[1], flor[1], bug[3]], [bug[1], bug[3],bug[0]],
+    [flor[3], flor[0], bug[1]], [flor[1], flor[2], flor[4]], [flor[1], flor[3], bug[4]]
+  ];
 
+  
 }
